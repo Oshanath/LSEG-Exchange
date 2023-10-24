666ae14125eae6dbe0b51c1d8b559e13938ca9e1
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-10-25 03:25:17 +0530

    add order logic structure complete

--- LSEG/Exchange/include/order_book.h
+++ LSEG/Exchange/include/order_book.h
@@ -7,15 +7,85 @@ class OrderBook
 {
 
 public:
-	inline void add_order(Order order)
+	inline void add_order(Order order, bool pfill = false)
 	{
+
+		auto sell_top = sell_orders.begin();
+		auto buy_top = buy_orders.begin();
+
 		if (order.side == BUY)
 		{
-			buy_orders.insert(BuyOrder(order));
+			if (sell_top == sell_orders.end() || order.price < sell_top->price)
+			{
+				// Normal Execution
+
+				buy_orders.insert(BuyOrder(order));
+				// generate report
+			}
+			else if (order.quantity != sell_top->quantity)
+			{
+				// PFill execution for either side
+
+				if (order.quantity < sell_top->quantity)
+				{
+					// generate fill report for new buy order
+					// generate pfill report for sell_top
+					sell_top->quantity -= order.quantity;
+				}
+				else
+				{
+					// generate fill report for sell_top
+					// generate pfill report for new buy order
+					sell_orders.erase(sell_top);
+					order.quantity = order.quantity - sell_top->quantity;
+					add_order(order, true);
+				}
+			}
+			else
+			{
+				// Fill execution
+
+				// generate report
+				// generate report
+				sell_orders.erase(sell_top);
+			}
 		}
 		else
 		{
-			sell_orders.insert(SellOrder(order));
+			if (buy_top == buy_orders.end() || order.price > buy_top->price)
+			{
+				// Normal Execution
+
+				sell_orders.insert(SellOrder(order));
+				// generate report
+			}
+			else if (order.quantity != buy_top->quantity)
+			{
+				// PFill execution for either side
+
+				if (order.quantity < buy_top->quantity)
+				{
+					// generate fill report for new sell order
+					// generate pfill report for buy_top
+					buy_top->quantity -= order.quantity;
+				}
+				else
+				{
+					// generate fill report for buy_top
+					// generate pfill report for new sell order
+					buy_orders.erase(buy_top);
+					order.quantity = order.quantity - buy_top->quantity;
+					add_order(order, true);
+				}
+			}
+			else
+			{
+				// Fill execution
+
+				// generate report
+				// generate report
+				buy_orders.erase(buy_top);
+			}
 		}
 	}
 
